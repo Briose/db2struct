ee0ac9999b5c0035e5a7f66a3d13d4de70a70d22
EricLi404 <leiflyy@outlook.com>
2020-01-14 21:02:38 +0800

    cg

--- cmd/db2struct/main.go
+++ cmd/db2struct/main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/Shelnutt2/db2struct"
+	"github.com/EricLi404/db2struct"
 	goopt "github.com/droundy/goopt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/howeyc/gopass"
